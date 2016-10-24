c9d6748ea343fa45f1708bf5501560636de26893
crnbrdrck <broderickciaran@gmail.com>
2016-10-24 11:57:49 +0100

    Need to text .py files on cs comps

--- cgi-bin/game_stats.py
+++ cgi-bin/game_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from cgitb import enable
 enable()
+from json import loads
 # Because the server runs in the same dir as this file, we don't need cookies
 
 """/*
@@ -9,12 +10,31 @@ enable()
 */"""
 
 # string: stats
-# The output from the statsfile written by the server
+# Player stats data from the server, put into a HTML table
 stats = ''
 
+# string: gameLength
+# The time the game took overall
+gameLength = ''
 try:
     statsfile = open('../stats/game_stats')
-    stats = statsfile.read()
+    data = loads(statsfile.read())
+    statsfile.close()
+    stats = ('<table class="table table-striped table-hover table-bordered">'
+             '<thead><tr><th class="text-center">Position</th>'
+             '<th class="text-center">User Name</th></tr></thead><tbody>')
+    players = data['players']
+    for i in range(len(players)):
+        player = players[i]
+        stats += '<tr style="color: %s;"><td>%i</td><td>%s</td></tr>' % (
+            player['colour'], i, player['username'])
+    stats += '</tbody></table>'
+
+    mins, secs = data['gameLength']
+    gameLength = ('<div class="label label-primary">'
+                  '<strong>Game Time:</strong> %i:%i'
+                  '</div>')
+    gameLength = gameLength % (mins, secs)
 except IOError:
     stats = '<h2>Stats not available</h2>'
 
@@ -24,15 +44,35 @@ print("""
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!--jQuery-->
+        <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <title>Arena - Stats</title>
     </head>
 
     <body>
-        <h1>Last Game Stats</h1>
-        <hr />
-        %s
-        <hr />
-        <a href="../index.html">Home</a>
+        <div class="container">
+            <h1 class="page-heading">Last Game Stats</h1>
+            %s
+
+            %s
+            <a class="btn btn-primary" href="../index.html">
+                <span class="glyphicon glyphicon-home"></span> 
+                Home
+            </a>
+        </div>
     </body>
 </html>
-""" % (stats))
+""" % (gameLength, stats))
--- cgi-bin/lobby.py
+++ cgi-bin/lobby.py
@@ -66,7 +66,8 @@ try:
         print()
         print(dumps({'players': data['players'], 'started': data['started']}))
     else:
-        players = '<table><thead><tr><th>User Name</th></tr></thead><tbody>'
+        players = ('<table class="table table-striped table-bordered">'
+                   '<thead><tr><th>User Name</th></tr></thead><tbody>'
         for player in data['players']:
             players += '<tr style="color: %s;"><td>%s</td></tr>' % (
                 player['colour'], player['userName'])
@@ -78,8 +79,22 @@ try:
         <!DOCTYPE html>
         <html>
             <head>
-                <title>Arena - Lobby</title>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+                <!-- Latest compiled and minified CSS -->
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+                <!-- Optional theme -->
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+                <!--jQuery-->
                 <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+
+                <!-- Latest compiled and minified JavaScript -->
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                <title>Arena - Lobby</title>
                 <script src="../scripts/lobby.js"></script>
             </head>
 
